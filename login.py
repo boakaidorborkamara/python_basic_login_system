b1f923aef4f98ed97a8749a2b1e3703693e06b97
Boakai <boakaidkamara@gmail.com>
2022-05-12 17:27:37 +0000

    added comments

--- login.py
+++ login.py
@@ -1,10 +1,18 @@
+#prompt user for name
 name = input("What is your name? \n")
+
+# database containing names of users who are allowed to use system
 allowed_users = ["James", "Mary", "Fatu", "Joy"]
+
+# default password for each user
 allowed_password = "Password"
 
+# check if the name the user enter is a valid name from the database
 if(name in allowed_users):
+    #prompt user to enter password
     password = input("Enter your password \n")
 
+    # check if the password is correct 
     if(password == allowed_password):
         print("Welcome")
 
